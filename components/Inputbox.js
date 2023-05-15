b5256b442bc0c0f19084e1f2bf36f99e98fad569
manlikexavy <chibokaxavier@gmail.com>
2023-05-15 10:07:27 -0700

    firebase working, able to add post , up next ading of image

--- components/Inputbox.js
+++ components/Inputbox.js
@@ -1,14 +1,26 @@
 import Image from "next/image";
-import React from "react";
+import React, { useRef } from "react";
 import { signOut, useSession } from "next-auth/react";
 import { EmojiHappyIcon } from "@heroicons/react/outline";
 import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid";
+import { db } from "../firebase";
+import { serverTimestamp, collection, addDoc } from "firebase/firestore";
 
 const Inputbox = () => {
-  const submitPost = (e) => {
+  const submitPost = async (e) => {
     e.preventDefault();
+    if (!inputRef.current.value) return;
+    await addDoc(collection(db, "posts"), {
+      Message: inputRef.current.value,
+      Name: session.user.name,
+      Email: session.user.email,
+      TimeStamp: serverTimestamp(),
+    });
+    inputRef.current.value = "";
   };
   const { data: session } = useSession();
+  const inputRef = useRef(null);
+
   return (
     <div className="bg-white p-2 text-gray-500 font-medium rounded-xl shadow-md mt-6">
       <div className="flex justify-center space-x-4 p-4">
@@ -19,18 +31,18 @@ const Inputbox = () => {
           height={40}
           width={40}
         />
-        <form className="flex flex-1 " >
+        <form className="flex flex-1 ">
           {" "}
           <input
             type=""
             placeholder={`What's on your mind ${session.user.name} ?`}
+            ref={inputRef}
             className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"
           />
           <button hidden onClick={submitPost} type="submit">
             Submit
           </button>
         </form>
-
       </div>
 
       <div className="flex justify-evenly  border-t p-3">
@@ -43,7 +55,6 @@ const Inputbox = () => {
           <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
         </div>
         <div className="inputIcon">
-
           <EmojiHappyIcon className="h-7 text-yellow-300 " />{" "}
           <p className="text-xs sm:text-sm xl:text-base">Feeling/Activity</p>
         </div>
--- firebase.js
+++ firebase.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import "firebase/storage";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +13,11 @@ const firebaseConfig = {
   projectId: "facebook-70e7b",
   storageBucket: "facebook-70e7b.appspot.com",
   messagingSenderId: "157064782643",
-  appId: "1:157064782643:web:9b517c9dc64ea90b861446"
+  appId: "1:157064782643:web:9b517c9dc64ea90b861446",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);;
+export { db, storage };
