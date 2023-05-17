47c5110268233170ce8b99928ef6c4f693b7b066
manlikexavy <chibokaxavier@gmail.com>
2023-05-17 05:50:15 -0700

    testing

--- components/Login.js
+++ components/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
      <div className='grid place-items-center'>
 
 <Image src="https://links.papareact.com/t4i" width={400} height={400}  style={{ objectFit: "contain" }}/>
-<h1 className='p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer' onClick={signIn}>Log in with FaceBook</h1>
+<h1 className='p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer' onClick={signIn}>Log in with Facebook </h1>
 </div>
     </>
    
